10da121729f651b37cb74f61340c5f06b1f12be6
Theads1 <tiaradheads@gmail.com>
2023-09-28 23:58:01 -0400

    wrong database...updated

--- db.js
+++ db.js
@@ -1,10 +1,14 @@
 const Pool= require("pg").Pool
 const pool= new Pool(
-  {user:"sundalandadmin",
-  password:"testing",
-  host:"localhost",
+  {user:"capstone_z1iz_user",
+  password:"pXwm5Vl1mScCZHiQzPDNuLNeRBAYzdaD",
+  host:"dpg-ckb2qm9kms5s73b5rcog-a.ohio-postgres.render.com",
   port:5432,
-  database:"sunlogin",}
+  database:"capstone_z1iz",
+  ssl : {
+    rejectUnauthorized: false
+  }
+}
     );
 
 module.exports = pool;
--- db/Product.sql
+++ db/Product.sql
@@ -9,6 +9,7 @@ CREATE TABLE products(
 );
 
 
+
 INSERT INTO products (title, price, description, category, img, rating) VALUES ('Luwint Children LED Finger Light Up Gloves - Amazing Colorful Glow Flashing Novelty Toys for Kids with Extra Batteries (Kids 5-12 Yrs Old)
 ', 11.69, 'GLOW IN THE DARK GLOVES FOR FUN AND SHOW,RELIABLE LIGHTING TECH & SOFT KNIT MATERIAL',
  'boys','https://m.media-amazon.com/images/I/61U5Rhwi6GL._AC_SX342_.jpg', 4.4) RETURNING *
